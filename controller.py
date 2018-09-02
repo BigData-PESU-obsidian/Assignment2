61332d90f51de22eccedffb54b26d60092b43d93
Yashas ND <yashasbharadwaj111@gmail.com>
2018-09-02 21:08:29 +0530

    Uses PID % 256 for master selection instead of sequence number, added safe stopping mechanism

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import os
 from kazoo.client import KazooClient
 
 zk = KazooClient(hosts="127.0.0.1:2181")
@@ -7,15 +8,16 @@ zk.start()
 
 try:
     zk.create("/assignment2")
+    zk.create("/stop")
 except Exception:
     pass
 
-zk.create("/assignment2/lck",
-          ephemeral=True,
-          sequence=True)
+nd = zk.create("/assignment2/lck", ephemeral=True, sequence=True)
+
+zk.set(nd, bytes([os.getpid() % 256]))
+zk.set("/stop", bytes([0]))
 
 print("Made the first seq znode.. start dynamic.py now")
-
 start = False
 
 while(not start):
@@ -32,7 +34,7 @@ while(not stop):
     ans = input("Stop simulation?(y/n) ")
     if(ans == "y"):
         zk.start()
-        zk.delete("/assignment2", recursive=True)
+        zk.set("/stop", bytes([1]))
         stop = True
     else:
         pass
--- dynamic.py
+++ dynamic.py
@@ -4,62 +4,75 @@ import sys
 import subprocess
 from kazoo.client import KazooClient
 
-# Initial connection establishment
-client = KazooClient(hosts="127.0.0.1:2181")
-client.start()
+try:
+    # Initial connection establishment
+    client = KazooClient(hosts="127.0.0.1:2181")
+    client.start()
 
-# create an ephemeral , sequence znode and get its name
-isMaster = False
-retval = client.create("/assignment2/lck",
-                       ephemeral=True,
-                       sequence=True).split("/")[2]
+    # If to stop the simulation
+    stop = bool(int.from_bytes(client.get("/stop")[0], byteorder="little"))
+    if(stop):
+        exit(0)
 
+    # create an ephemeral , sequence znode and get its name
+    isMaster = False
+    nodePath = client.create("/assignment2/lck", ephemeral=True, sequence=True)
 
-def masterFun1():
-    """
-    This is the function thats called as soon as one process becomes the
-    master
+    retval = nodePath.split("/")[2]
+    client.set(nodePath, bytes([os.getpid() % 256]))
 
-    WORKING : This creates a new process which again runs the same program
-              and after creation, the current master program terminates
-              after 1 second, thus releasing the lock and letting a new
-              process take over and continue
-    """
+    def masterFun1():
+        """
+        This is the function thats called as soon as one process becomes the
+        master
 
-    p = subprocess.Popen(['python', 'dynamic.py'])
-    print("New master {}".format(retval))
-    i = 0
-    while(i < 1):
+        WORKING : This creates a new process which again runs the same program
+                  and after creation, the current master program terminates
+                  after 1 second, thus releasing the lock and letting a new
+                  process take over and continue
+        """
+
+        p = subprocess.Popen(['python', 'dynamic.py'])
+        print("New master {}".format(retval))
+        i = 0
+        while(i < 1):
+            time.sleep(1)
+            i += 1
+        global isMaster
+        isMaster = True
+        client.delete("/assignment2/"+retval)
+
+    def onElection(election):
+        """
+        This is the listner function called on each of the non-master processes
+        as soon as the current master dies.
+
+        WORKING : This checks if the current process' PID is
+                  equal to the minimum thats present or not.
+                  if the current PID is equal to the minimum present in the
+                  list of sequential children nodes, then this function calls
+                  the above mentioned masterFun function else it resets its
+                  watcher on the current minimum znode.
+        """
+
+        lst = [(int.from_bytes(client.get("/assignment2/"+child)[0],
+                               byteorder="little"), child)
+               for child in client.get_children("/assignment2")]
+        lst.sort(key=lambda x: x[0])
+
+        if(lst[0][0] == (os.getpid() % 256)):
+            masterFun1()
+        else:
+            client.get("/assignment2/"+lst[0][1], watch=onElection)
+        return(0)
+
+    onElection(None)
+    stop = False
+
+    # Busy wait for chance to become next master
+    while(not isMaster):
         time.sleep(1)
-        i += 1
-    global isMaster
-    isMaster = True
-    client.delete("/assignment2/"+retval)
 
-
-def onElection(election):
-    """
-    This is the listner function called on each of the non-master processes
-    as soon as the current master dies.
-
-    WORKING : This checks if the current process' retval (the node id) is
-              equal to the minimum thats present or not.
-              if the current retval is equal to the minimum present in the
-              list of sequential children nodes, then this function calls
-              the above mentioned masterFun function else it resets its watcher
-              on the current minimum znode.
-    """
-    lst = client.get_children("/assignment2")
-    lst.sort()
-    if(lst[0] == retval):
-        masterFun1()
-    else:
-        client.get("/assignment2/"+lst[0], watch=onElection)
-    return(0)
-
-
-onElection(None)
-
-# Busy wait for chance to become next master
-while(not isMaster):
-    time.sleep(1)
+except Exception:
+    print("Stopping")
+    exit(0)
